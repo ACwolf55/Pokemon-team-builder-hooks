574723cf071b4c35995073e32ba662cc5610834d
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2024-07-24 17:29:02 -0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-      <h1>Pokemon Team Builder</h1>
-        
-    </header>
+    <header className="bg-gradient-to-r from-red-500 to-blue-500 w-full h-[15vh] flex items-center justify-center text-white px-4">
+    <img src="./pokeball.png" className="h-8 w-8 mr-2" alt="Pokeball" />
+    <h1 className="text-3xl font-bold">Pokemon Team Builder</h1>
+</header>
+
+
+
   )
 }
 
