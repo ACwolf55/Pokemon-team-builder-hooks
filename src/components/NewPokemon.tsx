162c72b81be029d193e58751484be259ffeb8e1e
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2024-07-25 13:16:36 -0400

    new pokemon base css

--- src/components/NewPokemon.tsx
+++ src/components/NewPokemon.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const NewPokemon = () => {
-  return (
-    <div>NewPokemon</div>
-  )
-}
+  const [inputPokemon, setInputPokemon] = useState("");
+  const [pokemon, setPokemon] = useState({});
 
-export default NewPokemon
+  const getPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${inputPokemon.toLowerCase()}`)
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  return (
+    <div className="w-full sm:w-1/2 lg:w-1/3 p-2">
+      <div className="border-2 border-gray-300 rounded-lg p-4 h-20 lg:h-[9.5rem]">
+        <div className="w-full sm:w-1/2 lg:w-1/3">
+          <div className="flex items-center space-x-2">
+          <input 
+        type="text" 
+        placeholder="Type Pokémon name!" 
+        value={inputPokemon}
+        onChange={(e) => setInputPokemon(e.target.value)}
+        className="p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 w-[200px] h-[25px] text-sm"
+    />
+    <button 
+        onClick={getPokemon}
+        className="bg-blue-500 text-white p-1 rounded-lg
+        w-[50px] h-[30px] text-xs"
+    >
+      Get!
+    </button>
+</div>
+            {pokemon && (
+              <div className="mt-2 w-[200px] h-[133px] overflow-auto">
+              <ul className="text-xs">
+                  <li>HP: {}</li>
+                  <li>Attack: {}</li>
+                  <li>Defense: {}</li>
+                  <li>Special Attack: {}</li>
+                  <li>Special Defense: {}</li>
+                  <li>Speed: {}</li>
+              </ul>
+          </div>
+            )}
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default NewPokemon;
--- src/components/NewPokemonTeam.tsx
+++ src/components/NewPokemonTeam.tsx
@@ -3,11 +3,27 @@ import NewPokemon from './NewPokemon'
 
 const NewPokemonTeam = () => {
   return (
-    <div className="lg:w-[60vw] w-full lg:h-[70vh] h-auto border-4 border-blue-500 rounded-lg p-4">
-        <h2>Make your pokemon Team!~</h2>
-        
-        
-     </div>
+    <div className="lg:w-[60vw] w-full lg:h-[75vh] h-auto border-4 border-blue-500 rounded-lg p-4">
+    <h2>Make your Pokémon Team!~</h2>
+    <div className="flex items-center space-x-2 mb-4">
+        <input 
+            type="text" 
+            placeholder="Team Name" 
+            className="w-1/4 p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" 
+        />
+        <button className="bg-blue-500 text-white p-2 rounded-lg">Save</button>
+    </div>
+    <div className="flex flex-wrap justify-between">
+       <NewPokemon/>
+       <NewPokemon/>
+       <NewPokemon/>
+       <NewPokemon/>
+       <NewPokemon/>
+       <NewPokemon/>
+    </div>
+</div>
+
+
   )
 }
 
